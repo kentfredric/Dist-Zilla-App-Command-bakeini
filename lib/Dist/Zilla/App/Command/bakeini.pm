80617167d3bd86a729bac8297f6ae40d580db896
Kent Fredric <kentfredric@gmail.com>
2017-03-04 19:04:39 +1300

    Build results of e8c1cb2 (on master)

--- lib/Dist/Zilla/App/Command/bakeini.pm
+++ lib/Dist/Zilla/App/Command/bakeini.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::App::Command::bakeini;
 
-our $VERSION = '0.002005';
+our $VERSION = '0.002006';
 
 # ABSTRACT: bake dist.ini to not need the bundles.
 
@@ -84,7 +84,7 @@ Dist::Zilla::App::Command::bakeini - bake dist.ini to not need the bundles.
 
 =head1 VERSION
 
-version 0.002005
+version 0.002006
 
 =head1 SYNOPSIS
 
@@ -227,7 +227,7 @@ Kent Fredric <kentnl@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2015 by Kent Fredric <kentfredric@gmail.com>.
+This software is copyright (c) 2017 by Kent Fredric <kentfredric@gmail.com>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
